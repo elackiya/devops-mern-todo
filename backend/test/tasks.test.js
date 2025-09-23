198b878b646c8f2bfc424e8b411abf1a9e5b3e8c
Elackiya <elackiya10@gmail.com>
2025-09-23 21:08:33 +0530

    CI testing file changes

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -8,12 +8,6 @@ describe('GET api/tasks', () => {
     expect(res.statusCode).toBe(200);
   })
 
-  it('it should return object and task property ok', async () => {
-    const res = await request(app).get('/api/tasks')
-    expect(typeof res.body).toBe("object");
-    expect(res.body).toHaveProperty("tasks")
-    console.log(res.body.tasks, 'Data SEEDED')
-  })
 })
 
 
