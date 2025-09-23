e039402715377fde5cd65340eac55da6fa244284
Elackiya <elackiya10@gmail.com>
2025-09-23 20:39:32 +0530

    CI workflow test case

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -2,12 +2,21 @@ const request = require('supertest');
 const {app,server} = require('../index');
 const mongoose = require('mongoose');
 //test suite
-describe('GET api/tasks', ()=>{
-it("it should return 200 ok", async()=>{
+describe('GET api/tasks', () => {
+  it("it should return 200 ok", async () => {
     const res = await request(app).get('/api/tasks')
     expect(res.statusCode).toBe(200);
+  })
+
+  it('it should return object and task property ok', async () => {
+    const res = await request(app).get('/api/tasks')
+    expect(typeof res.body).toBe("object");
+    expect(res.body).toHaveProperty("tasks")
+    console.log(res.body.tasks, 'Data SEEDED')
+  })
 })
-})
+
+
 
 afterAll(async()=>{
     await server.close();
